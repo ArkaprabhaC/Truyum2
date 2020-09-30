fcb527a67fc4a3f75b3eb48735f2ef16f9df2861
ArkaprabhaC <arkaprabha.chatterjee31@gmail.com>
2020-09-30 20:35:05 +0530

    added minimal swagger2 configuration

--- api-gateway/src/main/java/com/cts/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/cts/gateway/ApiGatewayApplication.java
@@ -6,8 +6,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import com.cts.gateway.exception.CustomErrorDecoder;
+import com.google.common.base.Predicate;
 
 import feign.codec.ErrorDecoder;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 
@@ -16,11 +23,12 @@ import feign.codec.ErrorDecoder;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
 public class ApiGatewayApplication {
-	
-	
+
 	/**
 	 * Application runner
+	 * 
 	 * @param args
 	 */
 	public static void main(final String[] args) {
@@ -30,5 +38,26 @@ public class ApiGatewayApplication {
 	@Bean
 	public ErrorDecoder errorDecoder() {
 		return new CustomErrorDecoder();
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		/*
+		 * return new Docket(DocumentationType.SWAGGER_2).select()
+		 * .apis(RequestHandlerSelectors.basePackage("com.cts")).build();
+		 */
+
+		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(apiInfo()).select()
+				.paths(myPaths()).build();
+	}
+
+	private Predicate<String> myPaths() {
+		return PathSelectors.regex("/api/.*");
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Truyum 2 API").description("Truyum API documentation")
+				.termsOfServiceUrl("https://cognizant.com").contact("arkaprabha.chatterjee@cognizant.com").license("Cognizant License")
+				.licenseUrl("arkaprabha.chatterjee@cognizant.com").version("1.0").build();
 	}
 }
